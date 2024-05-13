74fe9277d782762bd1768bdc62b53c67542a62e2
Mironova Lyudmila <mirlual@yandex.ru>
2024-05-13 18:27:54 +0400

    Upd str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def upper_case(s):
+    '''делает буквы в строке заглавными'''
     return s.title()
 
+
+def upper_first_letter(s):
+    s = ' '.join(word.capitalize() for word in s.split())
+    return s
